0bae71520ef59c4f92dca528776794a6a23f8741
metaphysika <34526637+metaphysika@users.noreply.github.com>
2018-09-05 08:12:27 -0500

    Removed extra "outputDir"
    Was in there by mistake.  Probably a copy and paste error.

--- Inbox_watcher.py
+++ Inbox_watcher.py
@@ -24,76 +24,67 @@ for f in os.listdir(importDir):
 """
 
 while True:
-    
+
     # daily_radfeedback_equip_report.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('daily_radfeedback_equip_report.csv'):
             shutil.move(oldName, py.path.local(r'H:\EPIC Rad Feedback\incoming_epic_rad_reports'))
-    
-    
+
     # TRF Dexa Repeat Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('TRF Dexa Repeat Data.csv'):
             shutil.move(oldName, py.path.local(r'W:\SHARE8 Physics\Reject Analysis\TRF MEDICAL CENTER\Dexa\Data'))
-    
+
     # Bagley Dexa Repeat Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('Bagley Dexa Repeat Data.csv'):
             shutil.move(oldName, py.path.local(r'W:\SHARE8 Physics\Reject Analysis\Bagley\DEXA\Data'))
-    
-    
+
     # Bagley CT Repeat Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('Bagley CT Repeat Data.csv'):
             shutil.move(oldName, py.path.local(r'W:\SHARE8 Physics\Reject Analysis\Bagley\CT\Data'))
-    
-    
+
     # Bemidji Clinic CT Repeat Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('Bemidji Clinic CT Repeat Data.csv'):
             shutil.move(oldName, py.path.local(r'W:\SHARE8 Physics\Reject Analysis\Bemidji Main\CT\Data'))
-    
-    
+
     # Bemidji Dexa Repeat Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('Bemidji Dexa Repeat Data.csv'):
             shutil.move(oldName, py.path.local(r'W:\SHARE8 Physics\Reject Analysis\Bemidji Main\DEXA\DATA'))
-    
-    
+
     # Bemidji ER TRT.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('Bemidji ER TRT.csv'):
             shutil.move(oldName, py.path.local(r'H:\Quality Improvement Projects\ER Turnaround Time\ER TRT DATA'))
-    
-    
+
     # Bemidji GRID Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('Bemidji GRID Data.csv'):
-            shutil.move(oldName, py.path.local(outputDir=r'H:\NRDR\GRID\Bemidji Data'))
-    
-    
+            shutil.move(oldName, py.path.local(r'H:\NRDR\GRID\Bemidji Data'))
+
     # Bemidji Hos CT Repeat Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('Bemidji Hos CT Repeat Data.csv'):
             shutil.move(oldName, py.path.local(r'W:\SHARE8 Physics\Reject Analysis\Bemidji Med Ctr\CT\data'))
-    
-    
+
     # TRF CT Repeat Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
         if f.endswith('TRF CT Repeat Data.csv'):
             shutil.move(oldName, py.path.local(r'W:\SHARE8 Physics\Reject Analysis\TRF MEDICAL CENTER\CT\Repeat Data'))
-    
-    
+
     # TRF GRID Data.csv
     for f in os.listdir(importDir):
         oldName = os.path.join(importDir, f)
